6da987b5c2301edb4876ff89498d30bb7b4741c7
Derek Nutile <dereknutile@gmail.com>
2014-06-02 06:54:24 -0700

    Scrollspy and transitions for top nav.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,13 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+// smooth scrolling
 $(function() {
     $('.scroll-to-section').bind('click', function(event) {
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
         event.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 1500);
     });
 });
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+// twitter bootstrap scrollspy
+$('body').scrollspy({target: '#scrollspy-links'})
