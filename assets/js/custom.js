abfd0d1b8ed0e81ee3f320daff1bb8740148a20a
Derek Nutile <dereknutile@gmail.com>
2016-03-09 06:06:38 -0800

    Removes unused javascript.  Tests masthead visibility.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,32 @@
-// smooth scrolling
-$(function() {
-    $('.scroll-to-section').bind('click', function(event) {
-        event.preventDefault();
-        var target = $(this).attr('href');
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 1500);
+$(document).ready(function() {
+
+    // smooth scrolling
+    $(function() {
+        $('.scroll-to-section').bind('click', function(event) {
+            event.preventDefault();
+            var target = $(this).attr('href');
+            $('html, body').stop().animate({
+                scrollTop: $(target).offset().top
+            }, 1500);
+        });
     });
-});
 
 // twitter bootstrap scrollspy
-$('body').scrollspy({target: '#scrollspy-links'})
+// $('body').scrollspy({target: '#scrollspy-links'})
+
+
+    // fix menu when passed
+    $('.masthead').visibility({
+      once: false,
+      onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+      },
+      onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+      }
+    })
+    ;
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+});
